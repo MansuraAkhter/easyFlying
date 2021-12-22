0ef86f082fb0fd6007fe01955c0d984358b6ceb4
MansuraAkhter <mansura3330@gmail.com>
2021-12-22 17:38:39 +0600

    add frontend and connect database

--- app.js
+++ app.js
@@ -1,4 +1,24 @@
 const express = require("express");
+const mysql = require("mysql");
+const dotenv = require('dotenv');
+
+dotenv.config({path: './.env'});
+
+
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB,
+});
+
+db.connect((error) => {
+    if(error){
+        console.log(error)
+    }else{
+        console.log("MYSQL Connected..")
+    }
+})
 
 
 const server = express();
